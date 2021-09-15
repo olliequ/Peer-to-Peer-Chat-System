98cc40872b1a023639860ccdd150adf3f36fd275
“Ollie” <oquarm@student.unimelb.edu.au>
2021-09-15 17:08:15 +1200

    test

--- client/src/main/java/com/kvoli/ClientMain.java
+++ client/src/main/java/com/kvoli/ClientMain.java
@@ -9,7 +9,7 @@ public class ClientMain {
         try {
             // Check args passed into server
             if (args.length == 0) {
-                System.out.println("Error: Please provide a server address.");
+                System.out.println("Error: Please provide a server address next time.");
             }
             else if (args.length == 1) {
                 String address = args[0];
