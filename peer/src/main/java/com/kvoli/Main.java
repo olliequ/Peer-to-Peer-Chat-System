23dcc22558214a5229b105ea18b0f17bc56804e6
stndn0 <49390281+stndn0@users.noreply.github.com>
2021-11-01 16:08:11 +1100

    Fixed bug when starting new peer

--- peer/src/main/java/com/kvoli/Main.java
+++ peer/src/main/java/com/kvoli/Main.java
@@ -9,9 +9,18 @@ public class Main {
         try {
             // If no ports are provided then the "server" port should be 4444 AND the default port is emphemeral.
             // Using port 0 tells the OS to randomly allocate a port.
+            // Example: java -jar chatpeer.jar
             if (args.length == 0) {
                 System.out.println("Attempting to build peer on server (listening) port 4444 and emphemeral port");
                 Peer peer = new Peer(defaultPort, 0);
+                peer.handle();
+            }
+
+            // User only provided -p
+            // Example: java -jar chatpeer.jar [-p port]
+            else if (args.length == 1) {
+                int port = Integer.parseInt(args[0]);
+                Peer peer = new Peer(port, 0);
                 peer.handle();
             }
 
